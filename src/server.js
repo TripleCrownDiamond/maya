085ff51158d2b1c3fab919de4142519e7bef9295
TripleCrownDiamond <gagbahungba2019@gmail.com>
2024-06-20 21:21:46 +0200

    correct server

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 
 app.all("/", (req, res) => {
-  res.send;
-  ("Bot is running");
+  res.send("Bot is running");
 });
 
 function keepAlive() {
